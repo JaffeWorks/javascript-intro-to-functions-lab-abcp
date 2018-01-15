3421d05b9218a174ab9a126018365f14fbef9d87
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-15 22:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ logShout(string){
   console.log(string.toUpperCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string){
+  if (string.toLowerCase() === string)
   return "I can't hear you"
-}}
+}
